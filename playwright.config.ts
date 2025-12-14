0ce7d0c668fb1895eb1c2718db6f23919a06eafb
Mauricio Miranda <maurimiranda@gmail.com>
2025-12-14 18:41:21 -0300

    Increase timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,10 +15,18 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "list",
+  /* Global test timeout */
+  timeout: 10000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "http://localhost:3000",
+
+    /* Default timeout for actions like click, fill, etc. */
+    actionTimeout: 10000,
+
+    /* Default timeout for navigation actions */
+    navigationTimeout: 10000,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
